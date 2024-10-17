968737fe2fd6466fdab86f76477666ac21fb8456
Juan Pablo Correa <54782677+dialextremo@users.noreply.github.com>
2024-10-17 15:36:38 -0500

    change shortcouts

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -16,6 +16,12 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+--Better navigation
+vim.keymap.set('n', 'H', '^', { desc = 'Move cursor to beginning of line' })
+vim.keymap.set('n', 'L', '$', { desc = 'Move cursor to end of line' })
+vim.keymap.set('n', 'J', '}', { desc = 'Move cursor to next space' })
+vim.keymap.set('n', 'K', '{', { desc = 'Move cursor to previous space' })
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -11,7 +11,13 @@ return {
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
-
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    }
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
